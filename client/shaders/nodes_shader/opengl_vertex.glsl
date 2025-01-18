6a6bdc30cebe8dd35c0d9be8ec2069037c587f0c
nonepro <noneprobanru@gmail.com>
2025-01-18 10:11:26 +0800

    优化球形渲染效果

--- client/shaders/nodes_shader/opengl_vertex.glsl
+++ client/shaders/nodes_shader/opengl_vertex.glsl
@@ -151,6 +151,17 @@ mat4 inverse(mat4 m) {
 		a20 * b03 - a21 * b01 + a22 * b00) / det;
 }
 
+/*
+ * Complex Number functions
+ */
+#define cplx vec2
+#define cplx_new(re, im) vec2(re, im)
+#define cplx_re(z) z.x
+#define cplx_im(z) z.y
+#define cplx_exp(z) (exp(z.x) * cplx_new(cos(z.y), sin(z.y)))
+#define cplx_scale(z, scalar) (z * scalar)
+#define cplx_abs(z) (sqrt(z.x * z.x + z.y * z.y))
+
 // OpenGL < 4.3 does not support continued preprocessor lines
 #if (MATERIAL_TYPE == TILE_MATERIAL_WAVING_LIQUID_TRANSPARENT || MATERIAL_TYPE == TILE_MATERIAL_WAVING_LIQUID_OPAQUE || MATERIAL_TYPE == TILE_MATERIAL_WAVING_LIQUID_BASIC) && ENABLE_WAVING_WATER
 
@@ -213,21 +224,17 @@ void main(void)
 #ifdef ENABLE_PLANET
 	mat4 viewModel = inverse(gl_ModelViewMatrix);
 	vec3 camPos = viewModel[3].xyz;
-	// Step 1: Transform normal coordinates into coordinates as if they were on a sphere
-	float radius = PLANET_RADIUS * BS * 16 + pos.y;
-	float xangle = (pos.x - camPos.x) / (PLANET_RADIUS * BS * 16);
-	float zangle = (pos.z - camPos.z) / (PLANET_RADIUS * BS * 16);
-	float distangle = pow(xangle * xangle + zangle * zangle, 0.5);
-	// Step 2: Transform back from spherical coordinates to cartesian system with
-	// the center of the planet in the origin of the coordinate system.
-	float planet_x = sin(xangle) * radius;
-	float planet_z = sin(zangle) * radius;
-	float planet_y = cos(distangle) * radius;
-	// Step 3: Translate coordinates so that they are relative to the camera, not the planet center
-	// The planet center is *always* position PLANET_RADIUS underneath the player
-	pos.y = planet_y - (PLANET_RADIUS * BS * 16);
-	pos.x = camPos.x + planet_x;
-	pos.z = camPos.z + planet_z;
+
+	float rp = PLANET_RADIUS * BS * 16;
+
+	// Naive approach
+	vec2 planedir = normalize(vec2(pos.x - camPos.x, pos.z - camPos.z));
+	vec2 plane = vec2(pos.y + rp, sqrt((pos.x - camPos.x) * (pos.x - camPos.x) + (pos.z - camPos.z) * (pos.z - camPos.z)));
+	vec2 circle = plane.x * vec2(cos(plane.y / rp), sin(plane.y / rp)) - vec2(rp, 0);
+	pos.x = circle.y * planedir.x + camPos.x;
+	pos.z = circle.y * planedir.y + camPos.z;
+	pos.y = circle.x;
+	
 #endif
 
 // OpenGL < 4.3 does not support continued preprocessor lines
